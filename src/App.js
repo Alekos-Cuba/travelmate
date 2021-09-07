151fb9cfa8d46ac5b7246fb02cb3d86f504fd900
Alekos <alekosmp86@gmail.com>
2021-09-07 17:43:29 -0400

    Added map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  MapConsumer,
+  MapContainer,
+  TileLayer,
+  ZoomControl,
+} from "react-leaflet";
+import NavBar from "./components/NavBar";
 
 function App() {
+  const defaultCenter = [38.9072, -77.0369];
+  const defaultZoom = 8;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar></NavBar>
+      <MapContainer
+        center={defaultCenter}
+        zoom={defaultZoom}
+        zoomControl={false}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <ZoomControl position="bottomright" />
+        <MapConsumer>
+          {(map) => {
+            window.navigator.geolocation.getCurrentPosition(
+              (res) => {
+                let latitude = res.coords.latitude,
+                  longitude = res.coords.longitude;
+                map.flyTo({ lat: latitude, lng: longitude }, 8);
+              },
+              (err) => {
+                alert("Failed to retrieve geolocation");
+              }
+            );
+            return null;
+          }}
+        </MapConsumer>
+      </MapContainer>
     </div>
   );
 }
