d8ced5acb9319948c230332690e2d59eb8e8fb7e
Alekos <alekosmp86@gmail.com>
2021-09-09 00:48:41 -0400

    Added styles for dropdown item

--- src/components/DropdownItem.js
+++ src/components/DropdownItem.js
@@ -1,9 +1,20 @@
+import "./../css/dropdownItem.css";
+import MapManager from "../scripts/MapManager";
+
 function DropdownItem(props) {
+  const getIcon = () => {
+    for (let value of MapManager.ICON_TYPES.values()) {
+      if (value.type === props.icon) return value.name;
+    }
+  };
   return (
     <div>
       <li>
         <a className="dropdown-item" href="#">
-          <i className={`bi bi-${props.icon}`}> - {props.title}</i>
+          <div className="dropdown-item-content">
+            <i className={`bi bi-${getIcon()}`}></i>
+            <span className="dropdown-item-title">{props.title}</span>
+          </div>
         </a>
       </li>
     </div>
