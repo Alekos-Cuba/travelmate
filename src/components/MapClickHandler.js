7e5d72abda183e5a1a6527fab331d0e584391f66
Alekos <alekosmp86@gmail.com>
2021-09-10 19:26:50 -0400

    Execute callback only if it is a valid function

--- src/components/MapClickHandler.js
+++ src/components/MapClickHandler.js
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { useMapEvent } from "react-leaflet";
 import MapManager from "../scripts/MapManager";
 
@@ -10,7 +9,7 @@ function MapClickHandler(props) {
       case MapManager.ACTIONS.ADD_MARKER: {
         MapManager.disableMapControls(map);
         MapManager.setCurrentAction(MapManager.ACTIONS.SET_MARKER_INFO);
-        props?.onMapClicked(lat, lng);
+        props.onMapClicked?.(lat, lng);
         break;
       }
       default: {
