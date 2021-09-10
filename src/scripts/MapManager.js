871fff5487648e3eeaf5d9d594b6d981890a04df
Alekos <alekosmp86@gmail.com>
2021-09-10 18:32:40 -0400

    Add asynchronous methods to get countries info

--- src/scripts/MapManager.js
+++ src/scripts/MapManager.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 class MapManagement {
   #currentAction;
   #map;
@@ -62,6 +64,26 @@ class MapManagement {
     this.#map.keyboard.enable();
     if (this.#map.tap) {
       this.#map.tap.enable();
+    }
+  }
+
+  async getCountries() {
+    try {
+      const countries = await axios.get(
+        "https://travelbriefing.org/countries.json"
+      );
+      return countries;
+    } catch (err) {
+      return { errorMessage: err.message };
+    }
+  }
+
+  async getCountryInfo(url) {
+    try {
+      const countryInfo = await axios.get(url);
+      return countryInfo;
+    } catch (err) {
+      return { errorMessage: err.message };
     }
   }
 }
