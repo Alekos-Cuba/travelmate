7af89f587670d508e7d9c5ad73c11ed63feadb52
Alekos <alekosmp86@gmail.com>
2021-09-11 13:48:58 -0400

    Refactored NavBar to support fast travel between locations

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,63 @@
 import "./../css/navbar.css";
-import DropdownMenu from "./DropdownMenu";
 import { useState } from "react";
+import axios from "axios";
+import DropdownMenu from "./../components/DropdownMenu";
 
-function NavBar() {
-  const [menu, setMenu] = useState([]);
+function NavBar(props) {
+  const [searchText, setSearchText] = useState();
+
+  const handleSearchTextChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    var options = {
+      method: "GET",
+      url: "https://trueway-places.p.rapidapi.com/FindPlaceByText",
+      params: { text: { searchText }, language: "en" },
+      headers: {
+        "x-rapidapi-host": "trueway-places.p.rapidapi.com",
+        "x-rapidapi-key": "8217908345msh55f2b71a7356f61p1e3d98jsn578f274acdf7",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-navbar">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          Menu
+        <a className="navbar-brand" href="#">
+          Tools
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDarkDropdown"
-          aria-controls="navbarNavDarkDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
         <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
           <ul className="navbar-nav">
-            {menu.map((m) => {
-              return (
-                <DropdownMenu
-                  key={m._id}
-                  title={m.title}
-                  items={m.items}
-                ></DropdownMenu>
-              );
-            })}
+            <DropdownMenu
+              title="Fast travel"
+              items={props.countryList}
+            ></DropdownMenu>
           </ul>
         </div>
+        <form className="d-flex">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            onChange={handleSearchTextChange}
+          />
+          <button className="btn btn-outline-success" onClick={handleSearch}>
+            Search
+          </button>
+        </form>
       </div>
     </nav>
   );
