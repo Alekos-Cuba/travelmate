dd486648ade9ae5fe5f44e39a7cc747d400dcc84
Alekos <alekosmp86@gmail.com>
2021-09-14 15:31:50 -0400

    Added offcanvas menu

---
+++ src/components/LeftMenu/OffcanvasMenu.js
@@ -0,0 +1,23 @@
+const OffcanvasMenu = (props) => {
+  return (
+    <div
+      className={`offcanvas offcanvas-${props.position} bg-dark text-white`}
+      tabIndex="-1"
+      id={props.id}
+      aria-labelledby={`${props.id}Label`}
+    >
+      <div className="offcanvas-header">
+        <h5 id={`${props.id}Label`}>{props.title}</h5>
+        <button
+          type="button"
+          className="btn-close text-reset"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div className="offcanvas-body">...</div>
+    </div>
+  );
+};
+
+export default OffcanvasMenu;
