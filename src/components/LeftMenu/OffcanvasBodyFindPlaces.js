92b14f5b24a79a1ec5b4bb7ad7c38d5aae475b2c
Alekos <alekosmp86@gmail.com>
2021-09-20 01:18:33 -0400

    Added method to handle the search of nearby places

--- src/components/LeftMenu/OffcanvasBodyFindPlaces.js
+++ src/components/LeftMenu/OffcanvasBodyFindPlaces.js
@@ -1,31 +1,62 @@
-import MapManager from "../../scripts/MapManager";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import ReadOnlyInput from "./ReadOnlyInput";
 
 const OffcanvasBodyFindPlaces = (props) => {
-  //const { lat, lng } = MapManager.getMap().options.center;
+  const mapCenter = useSelector((state) => state.mapCenter);
+
+  const handleFindButtonClick = (e) => {
+    e.preventDefault();
+    var options = {
+      method: "GET",
+      url: "https://trueway-places.p.rapidapi.com/FindPlacesNearby",
+      params: {
+        location: `${mapCenter.lat},${mapCenter.lng}`,
+        language: "en",
+        radius: "150",
+      },
+      headers: {
+        "x-rapidapi-host": "trueway-places.p.rapidapi.com",
+        "x-rapidapi-key": "8217908345msh55f2b71a7356f61p1e3d98jsn578f274acdf7",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response);
+        if (response.data.results?.length > 0) {
+          console.log(response.data);
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
   return (
     <>
       <div className="container">
         <form>
-          <label htmlFor="latInput">Latitude</label>
-          <input
+          <ReadOnlyInput
             id="latInput"
-            className="form-control"
             type="text"
-            aria-label="readonly input example"
-            readonly
-            disabled
-          ></input>
-        </form>
-        <form>
-          <label>Longitude</label>
-          <input
-            className="form-control"
+            value={mapCenter.lat}
+            label="Latitude"
+          />
+          <ReadOnlyInput
+            id="latInput"
             type="text"
-            aria-label="readonly input example"
-            readOnly
-            disabled
-          ></input>
+            value={mapCenter.lng}
+            label="Longitude"
+          />
+          <button
+            className="btn btn-success mt-2"
+            type="submit"
+            onClick={handleFindButtonClick}
+          >
+            Find
+          </button>
         </form>
       </div>
     </>
