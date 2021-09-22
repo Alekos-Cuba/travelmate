8dd457df095932ad67bb8b410212a11962690bee
Alekos <alekosmp86@gmail.com>
2021-09-22 00:46:31 -0400

    Allow to travel to nearby places using the shown results

--- src/components/LeftMenu/NearbyPlaceCard.js
+++ src/components/LeftMenu/NearbyPlaceCard.js
@@ -1,9 +1,23 @@
+import MapManager from "./../../scripts/MapManager";
+
 const NearbyPlaceCard = ({ data }) => {
+  const travelToPlace = () => {
+    MapManager.getMap().flyTo(
+      { lat: data.location.lat, lng: data.location.lng },
+      18
+    );
+  };
+
   return (
     <>
       <div className="card mt-2">
-        <div className="card-header bg-dark">
-          <b>{data.name}</b>
+        <div className="card-header d-flex flex-row justify-content-between bg-dark">
+          <span>
+            <b>{data.name}</b>
+          </span>
+          <a className="bg-dark" onClick={travelToPlace}>
+            <i className="bi bi-aspect-ratio"></i>
+          </a>
         </div>
         <div className="card-body bg-secondary d-flex flex-column">
           {data.address && <label>Address: {data.address}</label>}
