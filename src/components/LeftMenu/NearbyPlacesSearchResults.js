af350645928143d44af96a164b7f60181f6b8a38
Alekos <alekosmp86@gmail.com>
2021-09-22 00:14:11 -0400

    Moved render logic to function instead of being inside component's declaration

--- src/components/LeftMenu/NearbyPlacesSearchResults.js
+++ src/components/LeftMenu/NearbyPlacesSearchResults.js
@@ -1,18 +1,26 @@
 import styles from "./../../css/nearbyPlacesSearchResult.module.css";
+import UID from "../../scripts/IdGenerator";
 import { useSelector } from "react-redux";
 import NearbyPlaceCard from "./NearbyPlaceCard";
 
 const NearbyPlacesSearchResults = () => {
   const nearbyPlaces = useSelector((state) => state.nearbyPlaces);
 
+  const renderNearbyPlaces = () => {
+    let placesToRender = [];
+    if (nearbyPlaces.length === 0) {
+      placesToRender.push(<span key={UID.next().value}>No places nearby</span>);
+    }
+    nearbyPlaces.forEach((place) => {
+      placesToRender.push(<NearbyPlaceCard key={place.id} data={place} />);
+    });
+    return placesToRender;
+  };
+
   return (
     <>
       <div className={`container mt-4 ${styles.placesContainer}`}>
-        <div className="container-body">
-          {nearbyPlaces.map((place) => {
-            return <NearbyPlaceCard key={place.id} data={place} />;
-          })}
-        </div>
+        <div className="container-body">{renderNearbyPlaces()}</div>
       </div>
     </>
   );
