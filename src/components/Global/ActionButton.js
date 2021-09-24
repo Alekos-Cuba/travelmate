0b6ca256cb1d92e0c976d4bb14076d077d38e3ae
Alekos <alekosmp86@gmail.com>
2021-09-24 12:21:01 -0400

    Refactored button to make it more generic and reusable

---
+++ src/components/Global/ActionButton.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+function ActionButton({ action, icon, color, label }) {
+  const [isBusy, setIsBusy] = useState(false);
+
+  const hasLabel = () => {
+    return label !== "" && label !== null && label !== undefined;
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsBusy(true);
+    //append a callback function to original button action to handle change in states
+    action?.(() => {
+      setIsBusy(false);
+    });
+  };
+
+  return (
+    <button
+      type="button"
+      className={`ms-2 btn btn-${color ?? "primary"}`}
+      data-bs-toggle="button"
+      disabled={isBusy}
+      onClick={handleClick}
+    >
+      {isBusy ? (
+        <div>
+          <span
+            className={`spinner-border spinner-border-sm ${
+              hasLabel() && "me-2"
+            }`}
+            role="status"
+            aria-hidden="true"
+          ></span>
+          {label}
+        </div>
+      ) : (
+        <div>
+          <i className={`${hasLabel() && "me-2"} bi ${icon}`}></i>
+          {label}
+        </div>
+      )}
+    </button>
+  );
+}
+
+export default ActionButton;
