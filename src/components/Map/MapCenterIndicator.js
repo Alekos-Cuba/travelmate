aacd2c5568c2504f0a7eabd2e5ab61889f9a6acf
Alekos <alekosmp86@gmail.com>
2021-09-25 02:17:32 -0400

    Added functionality for updating map center coordinates

--- src/components/Map/MapCenterIndicator.js
+++ src/components/Map/MapCenterIndicator.js
@@ -1,6 +1,18 @@
 import IconProvider from "../../scripts/IconProvider";
+import { useDispatch } from "react-redux";
+import { useMapEvents } from "react-leaflet";
+import { setMapCenter } from "../../redux/actions/mapActions";
 
 const MapCenterIndicator = () => {
+  const dispatch = useDispatch();
+
+  const map = useMapEvents({
+    moveend() {
+      const { lat, lng } = map.getCenter();
+      dispatch(setMapCenter({ lat: lat.toFixed(3), lng: lng.toFixed(3) }));
+    },
+  });
+
   return (
     <div
       className={`bi ${IconProvider.getIconByName("plus")} z-index-550
